7d323726a232d0b2aea100d3c4175d07643ea136
IsaacNicaDev <chamorroeugenio5@gmail.com>
2023-03-08 13:04:44 -0600

    startapp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Button variant="contained">Boton sexy</Button>
+    </div>
+  )
+}
